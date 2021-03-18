9cd77f037caf03270861f18323428870736797a9
paulpelykh <eeddll1505@gmail.com>
2021-03-18 20:49:25 +0200

    Added Authentication Middleware

--- socialnetwork-funcitons/functions/index.js
+++ socialnetwork-funcitons/functions/index.js
@@ -20,6 +20,40 @@ firebase.initializeApp(config);
 
 const db = admin.firestore();
 
+const FBAuth = (req, res, next) => {
+	let idToken;
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.startsWith('Bearer ')
+	) {
+		idToken = req.headers.authorization.split('Bearer ')[1];
+	} else {
+		console.error('No token found');
+		return res.status(403).json({ error: 'Unathorized' });
+	}
+
+	admin
+		.auth()
+		.verifyIdToken(idToken)
+		.then((decodedToken) => {
+			req.user = decodedToken;
+			console.log(decodedToken);
+			return db
+				.collection('users')
+				.where('userId', '==', req.user.uid)
+				.limit(1)
+				.get();
+		})
+		.then((data) => {
+			req.user.handle = data.docs[0].data().handle;
+			return next();
+		})
+		.catch((err) => {
+			console.error('Error while verifying token ', err);
+			return res.status(403).json(err);
+		});
+};
+
 app.get('/posts', (req, res) => {
 	db.collection('posts')
 		.orderBy('createdAt', 'desc')
@@ -39,10 +73,10 @@ app.get('/posts', (req, res) => {
 		.catch((err) => console.error(err));
 });
 
-app.post('/post', (req, res) => {
+app.post('/post', FBAuth, (req, res) => {
 	const newPost = {
 		body: req.body.body,
-		userHandle: req.body.userHandle,
+		userHandle: req.user.handle,
 		createdAt: new Date().toISOString(),
 	};
 
