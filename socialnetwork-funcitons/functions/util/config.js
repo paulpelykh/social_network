51a9c6f3045d01b989a6d8d288cba88138974955
paulpelykh <eeddll1505@gmail.com>
2021-03-20 22:18:09 +0200

    Refactoring and organasing

---
+++ socialnetwork-funcitons/functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	apiKey: 'AIzaSyD96SZ5BzQx_CpXghUWlVm4q_JzWv26PtI',
+	authDomain: 'socialnework-2500b.firebaseapp.com',
+	projectId: 'socialnework-2500b',
+	storageBucket: 'socialnework-2500b.appspot.com',
+	messagingSenderId: '54603655443',
+	appId: '1:54603655443:web:ea5f252448a9e4514f92e6',
+	measurementId: 'G-C7G7FFX41Y',
+};
