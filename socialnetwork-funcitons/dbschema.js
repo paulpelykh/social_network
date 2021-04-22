7f25f1c12ad9eb0566e2a7c1ab4895aeb189cf3c
paulpelykh <eeddll1505@gmail.com>
2021-04-22 16:42:18 +0300

    Getting and commenting on post

--- socialnetwork-funcitons/dbschema.js
+++ socialnetwork-funcitons/dbschema.js
@@ -20,6 +20,14 @@ let db = {
 			commentCount: 2,
 		},
 	],
+	comments: [
+		{
+			userHandle: 'user',
+			postId: 'asldfjasdasdfasdf',
+			body: 'nice one mate!',
+			createdAt: '2020-03-15T10:59:52.798Z',
+		},
+	],
 };
 
 const userDetails = {
--- socialnetwork-funcitons/functions/index.js
+++ socialnetwork-funcitons/functions/index.js
@@ -1,10 +1,13 @@
 const functions = require('firebase-functions');
-
 const app = require('express')();
-
 const FBAuth = require('./util/fbAuth');
 
-const { getAllPosts, postOnePost } = require('./handlers/posts');
+const {
+	getAllPosts,
+	postOnePost,
+	getPost,
+	commentOnPost,
+} = require('./handlers/posts');
 const {
 	signup,
 	login,
@@ -16,6 +19,11 @@ const {
 //Post routes
 app.get('/posts', getAllPosts);
 app.post('/post', FBAuth, postOnePost);
+app.get('/post/:postId', getPost);
+//TODO: delete post
+//TODO: like a post
+//TODO: unliking a post
+app.post('/post/:postId/comment', FBAuth, commentOnPost);
 
 //users routes
 app.post('/signup', signup);
