446288525c044d8a2cae6ef16364c72d86033564
paulpelykh <eeddll1505@gmail.com>
2021-05-26 19:39:25 +0300

    Added Signup and Auth State

--- socialnetwork-client/src/App.js
+++ socialnetwork-client/src/App.js
@@ -1,34 +1,31 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import themeFile from './util/theme';
+import jwtDecode from 'jwt-decode';
 //Components
 import Navbar from './components/Navbar';
+import AuthRoute from './util/AuthRoute';
 //Pages
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
-const theme = createMuiTheme({
-	palette: {
-		primary: {
-			light: '#33c9dc',
-			main: '#00bcd4',
-			dark: '#008394',
-			contrastText: '#fff',
-		},
-		secondary: {
-			light: '#ff6333',
-			main: '#ff3d00',
-			dark: '#b22a00',
-			contrastText: '#fff',
-		},
-	},
-	typography: {
-		useNextVariants: true,
-	},
-});
+const theme = createMuiTheme(themeFile);
+
+let authenticated;
+const token = localStorage.FBIdToken;
+if (token) {
+	const decodedToken = jwtDecode(token);
+	if (decodedToken.exp * 1000 < Date.now()) {
+		window.location.href = '/login';
+		authenticated = false;
+	} else {
+		authenticated = true;
+	}
+}
 
 function App() {
 	return (
@@ -39,8 +36,18 @@ function App() {
 					<div className="container">
 						<Switch>
 							<Route exact path="/" component={home} />
-							<Route exact path="/login" component={login} />
-							<Route exact path="/signup" component={signup} />
+							<AuthRoute
+								exact
+								path="/login"
+								component={login}
+								authenticated={authenticated}
+							/>
+							<AuthRoute
+								exact
+								path="/signup"
+								component={signup}
+								authenticated={authenticated}
+							/>
 						</Switch>
 					</div>
 				</Router>
--- socialnetwork-client/src/pages/login.js
+++ socialnetwork-client/src/pages/login.js
@@ -12,33 +12,9 @@ import TextField from '@material-ui/core/Textfield';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const styles = {
-	form: {
-		textAlign: 'center',
-	},
-	image: {
-		width: '100px',
-		margin: '20px auto 20px auto',
-	},
-	pageTitle: {
-		margin: '10px auto 10px auto',
-	},
-	textField: {
-		margin: '10px auto 10px auto',
-	},
-	button: {
-		marginTop: 20,
-		position: 'relative',
-	},
-	customError: {
-		color: 'red',
-		fontSize: '0.8rem',
-		marginTop: 10,
-	},
-	progress: {
-		position: 'absolute',
-	},
-};
+const styles = (theme) => ({
+	...theme.spreadIt,
+});
 
 class login extends Component {
 	constructor() {
@@ -64,6 +40,7 @@ class login extends Component {
 			.post('/login', userData)
 			.then((res) => {
 				console.log(res.data);
+				localStorage.setItem('FBIdToken', `Berer ${res.data.token}`);
 				this.setState({
 					loading: false,
 				});
